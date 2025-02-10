871e1356e4cb8fe7b8d3254343738e76f930c97f
Paavan Shah <123231901+Paavanshah@users.noreply.github.com>
2025-02-10 20:55:03 +0530

    Add files via upload

---
+++ rover_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jetson/rover_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jetson/rover_ws/devel;/home/jetson/ydlidar_ws/devel;/opt/ros/noetic'.split(';') if '/home/jetson/rover_ws/devel;/home/jetson/ydlidar_ws/devel;/opt/ros/noetic' != '' else []
